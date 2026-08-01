384e942944ef0967fa890da6c62d92049245aeee
David Shorthose <jelly.master@gmail.com>
2026-08-02 00:00:27 +0100

    Hydrate retirement goals from storage

--- src/config/defaultRetirementGoals.ts
+++ src/config/defaultRetirementGoals.ts
@@ -1,9 +1,14 @@
 import type { RetirementGoals } from "../engine/models/RetirementGoals";
+import { loadStoredRetirementGoals } from "../state/retirementGoalsStorage";
 
-export const defaultRetirementGoals: RetirementGoals = {
+const fallbackRetirementGoals: RetirementGoals = {
   desiredAnnualIncome: 30_000,
   includeStatePension: true,
   statePensionAnnualAmount: 12_000,
   statePensionAge: 67,
   emergencyReserve: 20_000,
 };
+
+export const defaultRetirementGoals = loadStoredRetirementGoals(
+  fallbackRetirementGoals,
+);
