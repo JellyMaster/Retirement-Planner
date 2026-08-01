70182b6d4a7883492b33b8af765bf1568cb6c88a
David Shorthose <jelly.master@gmail.com>
2026-08-01 21:54:27 +0100

    Export scenario domain

---
+++ src/domain/scenarios/index.ts
@@ -0,0 +1,18 @@
+export {
+  DEFAULT_SCENARIO_COLOUR,
+  createBaselineScenario,
+  duplicateScenario,
+  type Scenario,
+  type ScenarioDependencies,
+  type ScenarioId,
+  type ScenarioState,
+} from "./Scenario";
+export {
+  LocalScenarioRepository,
+  SCENARIO_STORAGE_KEY,
+} from "./LocalScenarioRepository";
+export type { ScenarioRepository } from "./ScenarioRepository";
+export {
+  ScenarioService,
+  createBrowserScenarioDependencies,
+} from "./ScenarioService";
