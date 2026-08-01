bf542c051cacadf43457e9db66a15289a88c4043
David Shorthose <jelly.master@gmail.com>
2026-08-01 22:26:48 +0100

    Include scenario context in shared test renderer

--- src/test/renderWithProviders.tsx
+++ src/test/renderWithProviders.tsx
@@ -2,6 +2,7 @@ import type { ReactElement } from "react";
 import { render, type RenderOptions } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 
+import { ScenarioProvider } from "../components/scenarios";
 import { ThemeProvider } from "../theme/ThemeProvider";
 
 export function renderWithProviders(
@@ -11,7 +12,9 @@ export function renderWithProviders(
   return render(ui, {
     wrapper: ({ children }) => (
       <MemoryRouter>
-        <ThemeProvider>{children}</ThemeProvider>
+        <ThemeProvider>
+          <ScenarioProvider>{children}</ScenarioProvider>
+        </ThemeProvider>
       </MemoryRouter>
     ),
     ...options,
