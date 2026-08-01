6fb33dcce1b5eae096b215329c9ba9ab8f9a0cb8
David Shorthose <jelly.master@gmail.com>
2026-08-01 23:19:43 +0100

    Export scenario intelligence components

--- src/components/scenarios/index.ts
+++ src/components/scenarios/index.ts
@@ -1,5 +1,8 @@
 export { ScenarioChangesSummary } from "./ScenarioChangesSummary";
+export { ScenarioComparisonTabs } from "./ScenarioComparisonTabs";
 export { ScenarioEditModal } from "./ScenarioEditModal";
+export { ScenarioGrowthChart } from "./ScenarioGrowthChart";
+export { ScenarioIntelligencePanel } from "./ScenarioIntelligencePanel";
 export { ScenarioProvider } from "./ScenarioProvider";
 export { useScenarios } from "./ScenarioContext";
 export type { ScenarioContextValue } from "./ScenarioContext";
