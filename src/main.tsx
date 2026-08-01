a02ded154fb5db6d2b25e6d7c41d336b97868ee0
David Shorthose <jelly.master@gmail.com>
2026-08-01 22:48:25 +0100

    Load active scenario comparison styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './styles/scenario-comparison-active.css'
 import App from './App.tsx'
 import { ThemeProvider } from "./theme/ThemeProvider";
 
